2005345f04f5872bfda5f70601b3eb0958910e1c
Arhanace <arhanace@gmail.com>
2022-04-21 22:04:45 -0500

    write to firebase

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,25 @@
-import { hasConflict, getCourseTerm, getCourseNumber} from '/Users/arhansalunke/Desktop/CS394/scheduler/src/utilities/times.js';
+import { hasConflict, getCourseTerm, getCourseNumber, timeParts} from '/Users/arhansalunke/Desktop/CS394/scheduler/src/utilities/times.js';
+
+import { setData } from '/Users/arhansalunke/Desktop/CS394/scheduler/src/utilities/firebase.js';
+
+const reschedule = async (course, meets) => {
+  if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+    try {
+      await setData(`/courses/${course.id}/meets`, meets);
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
+
+
+const getCourseMeetingData = course => {
+  const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+  const valid = !meets || timeParts(meets).days;
+  if (valid) return meets;
+  alert('Invalid meeting data');
+  return null;
+};
 
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
@@ -6,10 +27,12 @@ const Course = ({ course, selected, setSelected }) => {
   const style = {
     backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
   };
+
   return (
     <div className="card m-1 p-2" 
-      style={style}
-      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+        style={style}
+        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
+        onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
       <div className="card-body">
         <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
         <div className="card-text">{ course.title }</div>
